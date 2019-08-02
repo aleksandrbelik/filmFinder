643c71884af137451bbc0152e48470e1d1062a06
Aleksandr Belik <aleksandrbelik@gmail.com>
2019-08-02 12:23:28 +0300

    use c

--- src/components/common/searchForm/searchForm.jsx
+++ src/components/common/searchForm/searchForm.jsx
@@ -6,50 +6,52 @@ import { Themes } from 'Utils/themes';
 import ThemeContext from 'Components/common/contextProvider';
 import './searchForm.less';
 
-const SearchForm = ({
-  onSubmit,
-  onChange,
-  value,
-  filter,
-  onFilterTitleClick,
-  onFilterGenreClick
-}) => (
-  <ThemeContext.Consumer>
-    {
-      theme => (
-        <form className={classNames('search-form', {
-          'theme-new-year': theme === Themes.NEW_YEAR,
-          'theme-summer': theme === Themes.SUMMER
-          })} onSubmit={onSubmit}>
-          <div className="search-form__field">
-            <span className="search-form__label">FIND YOUR MOVIE</span>
-            <input className="search-form__input" type="text" value={value} onChange={onChange} />
+class SearchForm extends React.Component {
+  static contextType = ThemeContext;
+
+  render() {
+    const {
+      onSubmit,
+      onChange,
+      value,
+      filter,
+      onFilterTitleClick,
+      onFilterGenreClick
+    } = this.props;
+    const theme = this.context;
+    return (
+      <form className={classNames('search-form', {
+        'theme-new-year': theme === Themes.NEW_YEAR,
+        'theme-summer': theme === Themes.SUMMER
+        })} onSubmit={onSubmit}>
+        <div className="search-form__field">
+          <span className="search-form__label">FIND YOUR MOVIE</span>
+          <input className="search-form__input" type="text" value={value} onChange={onChange} />
+        </div>
+        <div className="search-form__controls">
+          <div className="search-form__filter">
+            <span className="search-form__filter__label">SEARCH BY</span>
+            <button
+              className={classNames('search-form__filter__btn', { 'is-active': filter === searchTypes.TITLE })}
+              type="button"
+              onClick={onFilterTitleClick}
+            >
+              TITLE
+            </button>
+            <button
+              className={classNames('search-form__filter__btn', { 'is-active': filter === searchTypes.GENRE })}
+              type="button"
+              onClick={onFilterGenreClick}
+            >
+              GENRE
+            </button>
           </div>
-          <div className="search-form__controls">
-            <div className="search-form__filter">
-              <span className="search-form__filter__label">SEARCH BY</span>
-              <button
-                className={classNames('search-form__filter__btn', { 'is-active': filter === searchTypes.TITLE })}
-                type="button"
-                onClick={onFilterTitleClick}
-              >
-                TITLE
-              </button>
-              <button
-                className={classNames('search-form__filter__btn', { 'is-active': filter === searchTypes.GENRE })}
-                type="button"
-                onClick={onFilterGenreClick}
-              >
-                GENRE
-              </button>
-            </div>
-            <input className="search-form__button" type="submit" value="SEARCH" />
-          </div>
-        </form>
-      )
-    }
-  </ThemeContext.Consumer>
-);
+          <input className="search-form__button" type="submit" value="SEARCH" />
+        </div>
+      </form>
+    )
+  }
+}
 
 SearchForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
--- src/components/common/sorting/sorting.jsx
+++ src/components/common/sorting/sorting.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { sortTypes } from 'State/list';
+import { Themes } from 'Utils/themes';
+import ThemeContext from 'Components/common/contextProvider';
 import './sorting.less';
 
 const Sorting = ({
@@ -9,28 +11,34 @@ const Sorting = ({
   onSortRatingClick,
   sort,
   total
-}) => (
-  <div className="sorting">
-    <div className="sorting__found">{total} movies found</div>
-    <div className="sorting__controls">
-      <span className="sorting__controls__label">Sort by</span>
-      <button
-        className={classNames('sorting__button', { 'is-active': sort === sortTypes.RELEASE_DATE })}
-        type="button"
-        onClick={onSortReleaseDateClick}
-      >
-        release date
-      </button>
-      <button
-        className={classNames('sorting__button', { 'is-active': sort === sortTypes.RATING })}
-        type="button"
-        onClick={onSortRatingClick}
-      >
-        rating
-      </button>
+}) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <div className={classNames('sorting', {
+      'theme-new-year': theme === Themes.NEW_YEAR,
+      'theme-summer': theme === Themes.SUMMER
+      })}>
+      <div className="sorting__found">{total} movies found</div>
+      <div className="sorting__controls">
+        <span className="sorting__controls__label">Sort by</span>
+        <button
+          className={classNames('sorting__button', { 'is-active': sort === sortTypes.RELEASE_DATE })}
+          type="button"
+          onClick={onSortReleaseDateClick}
+        >
+          release date
+        </button>
+        <button
+          className={classNames('sorting__button', { 'is-active': sort === sortTypes.RATING })}
+          type="button"
+          onClick={onSortRatingClick}
+        >
+          rating
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 Sorting.propTypes = {
   sort: PropTypes.string.isRequired,
